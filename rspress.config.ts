f6cabeeae553c7182a263e73f466b16a601a40f4
sealday <sealday@gmail.com>
2024-12-26 18:17:06 +0800

    feat: support playground

--- rspress.config.ts
+++ rspress.config.ts
@@ -1,49 +1,60 @@
-import * as path from 'node:path';
-import { defineConfig } from 'rspress/config';
+import * as path from "node:path";
+import { defineConfig } from "rspress/config";
+import { pluginPreview } from "@rspress/plugin-preview";
+import { pluginPlayground } from "@rspress/plugin-playground";
 
 export default defineConfig({
-  root: path.join(__dirname, 'docs'),
-  title: 'Tachybase',
-  icon: '/tachybase-icon-light.png',
+  root: path.join(__dirname, "docs"),
+  plugins: [
+    pluginPreview({
+      defaultRenderMode: "pure",
+    }),
+    pluginPlayground({
+      defaultRenderMode: "pure",
+      defaultDirection: "vertical",
+    }),
+  ],
+  title: "Tachybase",
+  icon: "/tachybase-icon-light.png",
   logo: {
-    light: '/tachybase-light-blue.png',
-    dark: '/tachybase-dark-white.png',
+    light: "/tachybase-light-blue.png",
+    dark: "/tachybase-dark-white.png",
   },
   themeConfig: {
     socialLinks: [
       {
-        icon: 'github',
-        mode: 'link',
-        content: 'https://github.com/tachybase/tachybase',
+        icon: "github",
+        mode: "link",
+        content: "https://github.com/tachybase/tachybase",
       },
     ],
     locales: [
       {
-        lang: 'en',
-        label: 'dd',
-        outlineTitle: 'ON THIS Page',
+        lang: "en",
+        label: "dd",
+        outlineTitle: "ON THIS Page",
       },
       {
-        lang: 'zh',
-        label: 'dd',
-        outlineTitle: '大纲',
+        lang: "zh",
+        label: "dd",
+        outlineTitle: "大纲",
       },
     ],
   },
-  lang: 'zh',
+  lang: "zh",
   locales: [
     {
-      lang: 'en',
+      lang: "en",
       // 导航栏切换语言的标签
-      label: 'English',
-      title: 'Rspress',
-      description: 'Static Site Generator',
+      label: "English",
+      title: "Rspress",
+      description: "Static Site Generator",
     },
     {
-      lang: 'zh',
-      label: '简体中文',
-      title: 'Rspress',
-      description: '静态网站生成器',
+      lang: "zh",
+      label: "简体中文",
+      title: "Rspress",
+      description: "静态网站生成器",
     },
   ],
 });
