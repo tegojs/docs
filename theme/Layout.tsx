c140b50eba878c4aab6cd5d886ee4bcc7967986f
bai.zixv <602250248@qq.com>
2025-08-06 13:59:00 +0800

    feat: style update and footer update

--- theme/Layout.tsx
+++ theme/Layout.tsx
@@ -1,49 +1,54 @@
-import { Layout as BasicLayout } from "@rspress/theme-default";
-import { useEffect } from "react";
+import { usePageData } from '@rspress/core/runtime'
+import { Layout as BasicLayout } from '@rspress/theme-default'
+import { useEffect } from 'react'
 
 // 以下展示所有的 Props
 const Layout = () => {
+  const { page } = usePageData()
+  const isEnglish = page.lang === 'en'
   useEffect(() => {
-    const script = document.createElement("script");
-    script.src = "https://utteranc.es/client.js";
-    script.setAttribute("repo", "tegojs/docs");
-    script.setAttribute("issue-term", "pathname");
-    script.setAttribute("theme", "github-light");
-    script.crossOrigin = "anonymous";
-    script.async = true;
+    const script = document.createElement('script')
+    script.src = 'https://utteranc.es/client.js'
+    script.setAttribute('repo', 'tegojs/docs')
+    script.setAttribute('issue-term', 'pathname')
+    script.setAttribute('theme', 'github-light')
+    script.crossOrigin = 'anonymous'
+    script.async = true
 
-    const commentsEl = document.getElementById("utterances-container");
+    const commentsEl = document.getElementById('utterances-container')
     if (commentsEl) {
-      commentsEl.innerHTML = ""; // 清空旧内容（避免重复加载）
-      commentsEl.appendChild(script);
+      commentsEl.innerHTML = '' // 清空旧内容（避免重复加载）
+      commentsEl.appendChild(script)
     }
-  }, []);
+  }, [])
   return (
     <BasicLayout
       afterDocContent={<div id="utterances-container" />}
       bottom={
-        // TODO: 多国语言 + 样式优化
-        <div style={{ textAlign: "center" }}>
+        <div className="pb-5" style={{ textAlign: 'center' }}>
           <span id="busuanzi_container_site_pv">
-            本站总访问量&nbsp;
-            <span id="busuanzi_value_site_pv" />&nbsp;次
+            {isEnglish ? 'Total visits' : '本站总访问量'}&nbsp;
+            <span id="busuanzi_value_site_pv" />
+            &nbsp;{isEnglish ? 'times' : '次'}
           </span>
           &nbsp; &nbsp; &nbsp;
           <span id="busuanzi_container_site_uv">
-            本站访客数&nbsp;
+            {isEnglish ? 'Total visitors' : '本站访客数'}&nbsp;
             <span id="busuanzi_value_site_uv" />
-            &nbsp;人次
+            &nbsp;{isEnglish ? 'times' : '人次'}
           </span>
           &nbsp; &nbsp; &nbsp;
           <span id="busuanzi_container_page_pv">
-            本文总阅读量&nbsp;
-            <span id="busuanzi_value_page_pv" />&nbsp;次
+            {isEnglish ? 'Total reading' : '本文总阅读量'}&nbsp;
+            <span id="busuanzi_value_page_pv" />
+            &nbsp;{isEnglish ? 'times' : '次'}.
           </span>
-          &nbsp;&nbsp;&nbsp;Powered by Tego Team
+          &nbsp;&nbsp;&nbsp;
+          {isEnglish ? 'Powered by Tego Team' : '由零几团队提供技术支持'}
         </div>
       }
     />
-  );
-};
+  )
+}
 
-export { Layout };
+export { Layout }
